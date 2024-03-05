7be638b8af39b97f884334a0f5e7c1416f0db6af
SanRM <SanRM@users.noreply.github.com>
2024-03-05 15:40:25 +0000

    deploy: 2be8737bf01332d77c337ccde613dc229e2092cb

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -59,7 +59,7 @@ function initSearch() {
     if (request.status >= 200 && request.status < 400) {
       var docs = JSON.parse(request.responseText);
       
-      lunr.tokenizer.separator = /[\s\-/]+/
+      lunr.tokenizer.separator = /[\s/]+/
 
       var index = lunr(function(){
         this.ref('id');
@@ -236,7 +236,7 @@ function searchLoaded(index, docs) {
             var previewEnd = position[0] + position[1];
             var ellipsesBefore = true;
             var ellipsesAfter = true;
-            for (var k = 0; k < 5; k++) {
+            for (var k = 0; k < 3; k++) {
               var nextSpace = doc.content.lastIndexOf(' ', previewStart - 2);
               var nextDot = doc.content.lastIndexOf('. ', previewStart - 2);
               if ((nextDot >= 0) && (nextDot > nextSpace)) {
@@ -251,7 +251,7 @@ function searchLoaded(index, docs) {
               }
               previewStart = nextSpace + 1;
             }
-            for (var k = 0; k < 10; k++) {
+            for (var k = 0; k < 3; k++) {
               var nextSpace = doc.content.indexOf(' ', previewEnd + 1);
               var nextDot = doc.content.indexOf('. ', previewEnd + 1);
               if ((nextDot >= 0) && (nextDot < nextSpace)) {
@@ -311,7 +311,7 @@ function searchLoaded(index, docs) {
         resultLink.appendChild(resultPreviews);
 
         var content = doc.content;
-        for (var j = 0; j < Math.min(previewPositions.length, 3); j++) {
+        for (var j = 0; j < Math.min(previewPositions.length, 2); j++) {
           var position = previewPositions[j];
 
           var resultPreview = document.createElement('div');
